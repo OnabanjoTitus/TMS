39eb66f40461cffd97ed24aa12cf34ab33d5e596
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-04-30 08:35:50 +0100

    updated

--- src/test/java/repository/ProjectRepoImplTest.java
+++ src/test/java/repository/ProjectRepoImplTest.java
@@ -1,37 +1,37 @@
-//package repository;
-//
-//import Service.ProjectService;
-//import Service.ProjectServiceImpl;
-//import model.Project;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-//
-//import java.time.LocalDate;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringJUnitConfig
-//class ProjectRepoImplTest {
-//    @Autowired
-//    private final ProjectService projectService;
-//    @Autowired
-//    public ProjectRepoImplTest(ProjectService projectService) {
-//        this.projectService=projectService;
-//    }
-//
-//
-//    @Test
-//    void checkIfSaved(){
-//        Project newProject =new Project(1L,"first", LocalDate.now());
-//         assertNotNull(projectRepo.save(newProject));
-//    }
-//    @Test
-//    void checkThatItCanBeFetched(){
-//        Project newProject =new Project(1L,"first", LocalDate.now());
-//        projectRepo.save(newProject);
-//        assertEquals(newProject,projectRepo.findById(1L).get());
-//    }
-//}
+package repository;
+
+import Service.ProjectService;
+import Service.ProjectServiceImpl;
+import model.Project;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig
+class ProjectRepoImplTest {
+    @Autowired
+    private final ProjectService projectService;
+    @Autowired
+    public ProjectRepoImplTest(ProjectService projectService) {
+        this.projectService=projectService;
+    }
+
+
+    @Test
+    void checkIfSaved(){
+        Project newProject =new Project(1L,"first", LocalDate.now());
+         assertNotNull(projectRepo.save(newProject));
+    }
+    @Test
+    void checkThatItCanBeFetched(){
+        Project newProject =new Project(1L,"first", LocalDate.now());
+        projectRepo.save(newProject);
+        assertEquals(newProject,projectRepo.findById(1L).get());
+    }
+}
